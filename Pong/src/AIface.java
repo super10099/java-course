49f23ff415a7b0b349174442c2bc5389622e142a
799196 <799196@MS-HSRM401-5.MAISDAD.LOCAL>
2017-12-12 09:45:20 -0500

    added skynet v1

---
+++ Pong/src/AIface.java
@@ -0,0 +1,7 @@
+
+public interface AIface{
+	
+	String getDir();
+	void setPaddle(Paddle paddle);
+	void setBall(Ball ball);
+}
---
+++ Pong/src/Botv1.java
@@ -0,0 +1,34 @@
+
+public class Botv1 implements AIface{
+	Paddle paddle;
+	Ball ball;
+	
+	@Override
+	public String getDir() {
+		double centerOfPaddle = paddle.y+paddle.height/2;
+		double ballY = ball.getCenterY();
+		if (centerOfPaddle > ballY) {
+			System.out.println("up");
+			return "up";
+		} else if (centerOfPaddle < ballY){
+			System.out.println("down");
+			return "down";
+			
+		} else {
+			return "null";
+		}
+	}
+
+	@Override
+	public void setPaddle(Paddle paddle) {
+		// TODO Auto-generated method stub
+		this.paddle = paddle;
+	}
+
+	@Override
+	public void setBall(Ball ball) {
+		// TODO Auto-generated method stub
+		this.ball = ball;
+	}
+
+}
